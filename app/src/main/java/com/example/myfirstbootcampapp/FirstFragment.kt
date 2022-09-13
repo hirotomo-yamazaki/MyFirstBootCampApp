f86cb53c8cd01a2bd8c58009fc176296a326559f
hirotomo-yamazaki <hirotomo.yamazaki@gmail.com>
2022-09-13 09:49:56 +0900

    作成

--- app/src/main/java/com/example/myfirstbootcampapp/FirstFragment.kt
+++ app/src/main/java/com/example/myfirstbootcampapp/FirstFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.myfirstbootcampapp.databinding.FragmentFirstBinding
 
@@ -32,9 +35,48 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        /**
+         * 記述コード
+         */
+        binding.randomButton.setOnClickListener {
+            val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+            val currentCount = showCountTextView.text.toString().toInt()
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount)
+            findNavController().navigate(action)
         }
+
+        /**
+         *記述コード
+         */
+        binding.toastButton.setOnClickListener {
+            val myToast = Toast.makeText(context, R.string.toast_msg, Toast.LENGTH_SHORT)
+            myToast.show()
+        }
+
+        /**
+         *記述コード
+         */
+        view.findViewById<Button>(R.id.count_button).setOnClickListener {
+            countMe(view)
+        }
+    }
+
+    /**
+     * 記述コード
+     */
+    private fun countMe(view: View){
+        // Get the text view
+        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+
+        //Get the value of the text view
+        val countString = showCountTextView.text.toString()
+
+        // Convert value to a number and increment it
+        var count = countString.toInt()
+        count++
+
+        // Display the new value in the text view
+        showCountTextView.text = count.toString()
     }
 
     override fun onDestroyView() {
